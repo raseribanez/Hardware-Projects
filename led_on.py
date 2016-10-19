061e098c3a1ba7a49c0cee92e6b22981136b3cbd
Ben Woodfield <raser.13@hotmail.com>
2016-10-19 15:51:05 +0100

    Turn GPIO pins ON

--- led_on.py
+++ led_on.py
@@ -1,9 +1,12 @@
 # Ben Woodfield
-# Pin Activate - quick script
+# GPIO Pin Activation - quick script
+# Turns pins/LED/Hardware ON constantly
 
 import RPi.GPIO
 import time
 
+# Here I activate GPIO 23 and GPIO 4
+# Change this if needed to your desired Pins / See my RPi Pinout chart - or your specific RPi model GPIO chart
 RPi.GPIO.setmode(RPi.GPIO.BCM)
 RPi.GPIO.setup(23, RPi.GPIO.OUT)
 RPi.GPIO.setup(4, RPi.GPIO.OUT)
